31f4bc5ad46420ffb1f88dbd9851c5a9be43082c
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-24 14:17:14 +0530

    USE-> rug=add lines on x-axis

---
+++ Histogram_Plot/4_problem.py
@@ -0,0 +1,10 @@
+# Histogram plot in seaborn and organized data using bins
+# kde -> kernal density estimator
+# rug -> add line on x-axis
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+load_data = sns.load_dataset("penguins")
+sns.displot(load_data["bill_length_mm"],bins=[35,40,45,50,55,65],kde=True , rug=True)
+plt.show()

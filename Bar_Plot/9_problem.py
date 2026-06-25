adfed1a72e9bd5760d4b2d6964e1feb7f099fbeb
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-25 16:09:00 +0530

    Change color or saturation

--- Bar_Plot/9_problem.py
+++ Bar_Plot/9_problem.py
@@ -1,10 +1,10 @@
-# Draw horizontal and change color using palette
+# change color using palette and change saturation
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import pandas as pd
 
-load_data = sns.load_dataset("penguins").head(50)
+load_data = sns.load_dataset("penguins").head(10)
 
 order_1 = ["Dream","Torgersen" , "Biscoe"]
-sns.barplot(x="bill_depth_mm" , y="bill_length_mm" , data=load_data ,orient="h" ,hue="sex" , palette='PuBu')
+sns.barplot(x="bill_depth_mm" , y="bill_length_mm" , data=load_data ,orient="h" ,hue="sex" , palette='PuBu' , saturation=0.3)
 plt.show()

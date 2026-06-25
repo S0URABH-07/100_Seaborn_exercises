fc647952bd069650a267fedc32ff0fcdeb848d63
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-25 16:22:33 +0530

    add cap on bars

---
+++ Bar_Plot/11_problem.py
@@ -0,0 +1,10 @@
+# Bar plot and add cap on bars
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pandas as pd
+
+load_data = sns.load_dataset("penguins").head(100)
+
+order_1 = ["Dream","Torgersen" , "Biscoe"]
+sns.barplot(x="island" , y="bill_length_mm" , data=load_data , hue="sex" , order=order_1 ,hue_order=["Female","Male"], capsize=0.4)
+plt.show()

240b588d40f84afa12b1793f7f47a00a515fdab5
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-25 15:47:01 +0530

    Change order of hue

---
+++ Bar_Plot/5_problem.py
@@ -0,0 +1,10 @@
+# Bar plot and change order of hue
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pandas as pd
+
+load_data = sns.load_dataset("penguins").head(100)
+
+order_1 = ["Dream","Torgersen" , "Biscoe"]
+sns.barplot(x="island" , y="bill_length_mm" , data=load_data , hue="sex" , order=order_1 ,hue_order=["Female","Male"])
+plt.show()

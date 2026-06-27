594c5f7f1b9198492706ad8adc9f75cea039de64
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-27 19:44:03 +0530

    Add linewidth or linecolor

---
+++ Violin_Plot/4_problem.py
@@ -0,0 +1,8 @@
+# Violin plot with linewidth or linecolor
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+load_data = sns.load_dataset("penguins")
+sns.violinplot(x="species", y="bill_length_mm",data=load_data,linewidth=2 , linecolor="black" )
+
+plt.show()

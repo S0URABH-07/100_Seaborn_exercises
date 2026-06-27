ab06c5232078b3073f3d5072ee8c767b7427a435
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-27 10:52:41 +0530

    Change font or color And also remove x,y axis labels

--- Heatmap_Plot/12_problem.py
+++ Heatmap_Plot/12_problem.py
@@ -1,5 +1,5 @@
 # Change fontsize or color of annot using annot_kws
-# Rows and columns are labeled automatically
+# Remove lavels of x and y axis
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,5 +7,5 @@ import pandas as pd
 load_data = sns.load_dataset("penguins").drop(["species","island","sex"] , axis=1).head(10)
 
 fontcolor = {"fontsize":10 , "color":"r"}
-sns.heatmap(load_data,annot=True ,vmin=0,vmax=2000 ,linewidths=2, linecolor="r", annot_kws=fontcolor)
+sns.heatmap(load_data,annot=True ,vmin=0,vmax=2000 ,linewidths=2, linecolor="r", annot_kws=fontcolor ,xticklabels=False , yticklabels=False)
 plt.show()

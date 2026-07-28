337bd41b4749de1f9c34a4eec347bba0e1c7d727
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-28 22:45:12 +0530

    Bar Plot

---
+++ Analyst/plot16.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("HR_Analytics.csv")
+
+salary_std = (df.groupby("Department").agg(
+          AverageSalary=("MonthlyIncome", "mean"),
+          SalaryVariation=("MonthlyIncome", "std")
+      ).sort_values("SalaryVariation", ascending=False)
+)
+plt.figure(figsize=(8,5))
+
+sns.barplot(
+    data=salary_std.reset_index(),
+    x="Department",
+    y="SalaryVariation"
+)
+
+plt.title("Salary Variation by Department")
+
+plt.show()

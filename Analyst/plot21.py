bbe6cd4cd5e38396f6d0b11d13437826138ab7ab
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-08-01 22:46:07 +0530

    Bar Plot

--- Analyst/plot21.py
+++ Analyst/plot21.py
@@ -3,14 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("HR_Analytics.csv")
-travel = (
-    df.groupby("BusinessTravel").agg(
-          AttritionRate=(
-              "Attrition",
-              lambda x: (x == "Yes").mean() * 100
-          )
-      ).reset_index()
-)
+travel = (df.groupby("BusinessTravel").agg(AttritionRate=("Attrition",lambda x: (x == "Yes").mean() * 100)).reset_index())
 plt.figure(figsize=(8,5))
 
 sns.barplot(

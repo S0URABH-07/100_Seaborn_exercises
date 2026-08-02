9ad5312557553def8b22fc2b4d49700c344b1184
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-08-02 23:28:09 +0530

    Bar Plot

--- Analyst/plot18.py
+++ Analyst/plot18.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("HR_Analytics.csv")
-overtime = (
-    df.groupby("JobRole").agg(OvertimeRate=("OverTime",lambda x: (x == "Yes").mean() * 100)).sort_values("OvertimeRate", ascending=False).reset_index())
+overtime = (df.groupby("JobRole").agg(OvertimeRate=("OverTime",lambda x: (x == "Yes").mean() * 100)).sort_values("OvertimeRate",ascending=False).reset_index())
 plt.figure(figsize=(12,6))
 
 sns.barplot(

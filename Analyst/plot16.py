493091f8e683ed55fc548aa02d4235cb5015afea
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-08-04 03:39:42 +0530

    Bar Plot

--- Analyst/plot16.py
+++ Analyst/plot16.py
@@ -4,11 +4,7 @@ import seaborn as sns
 
 df = pd.read_csv("HR_Analytics.csv")
 
-salary_std = (df.groupby("Department").agg(
-          AverageSalary=("MonthlyIncome", "mean"),
-          SalaryVariation=("MonthlyIncome", "std")
-      ).sort_values("SalaryVariation", ascending=False)
-)
+salary_std = (df.groupby("Department").agg(AverageSalary=("MonthlyIncome", "mean"),SalaryVariation=("MonthlyIncome", "std")).sort_values("SalaryVariation", ascending=False))
 plt.figure(figsize=(8,5))
 
 sns.barplot(
